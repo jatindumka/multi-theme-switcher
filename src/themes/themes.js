0bedda9870e203ca2019e486574ed3e13d78fba0
jatin <jatindumka@gmail.com>
2025-07-30 15:27:47 +0530

    Themes defined

---
+++ src/themes/themes.js
@@ -0,0 +1,14 @@
+export const themes = {
+  theme1: {
+    font: "Arial, sans-serif",
+    background: "#f9f9f9",
+  },
+  theme2: {
+    font: "Georgia, serif",
+    background: "#1c1c1c",
+  },
+  theme3: {
+    font: "'Pacifico', cursive",
+    background: "#ffe4e1",
+  },
+};
