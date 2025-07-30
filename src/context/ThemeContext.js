be985a3a5cba86049efda3434ddb251ee036459e
jatin <jatindumka@gmail.com>
2025-07-30 15:25:36 +0530

    Created ThemeContexProvide

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "theme1";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <div className={`app ${theme}`}>{children}</div>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
